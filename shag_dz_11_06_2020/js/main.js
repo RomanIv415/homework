4846bfa6a4f47238b4e61d06e8cefe29e760c0ad
RomanIV415 <romaivanov-00@mail.ru>
2020-06-11 13:27:51 +0300

    V4

---
+++ shag_dz_11_06_2020/js/main.js
@@ -0,0 +1,3 @@
+let color = document.querySelector('body');
+let checkk = document.querySelector('.switcher-checkbox');
+checkk.checked ? color.classList.add('night') : color.classList.add('white');
